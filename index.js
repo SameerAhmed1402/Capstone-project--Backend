92a7c0919f1db9895d1c37aca3226b0a77b7141e
SameerAhmed1402 <sameerkuraishi1402@gmail.com>
2024-05-14 11:08:13 +0530

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://luxury-puffpuff-fcd753.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
